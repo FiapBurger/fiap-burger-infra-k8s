157f68ea101bd18904355135c0f65a557c037073
FiapBurger <fiapburger.tech@gmail.com>
2024-03-18 16:17:19 -0300

    feat: adicionando loadbalance

--- main.tf
+++ main.tf
@@ -14,6 +14,61 @@ resource "aws_eks_cluster" "fiap_cluster" {
   }
 }
 
+resource "aws_security_group" "eks_lb_sg" {
+  name        = "eks-lb-sg"
+  description = "Security group for EKS load balancer"
+  vpc_id      = aws_eks_cluster.fiap_cluster.vpc_config[0].vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "eks_lb" {
+  name               = "eks-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_eks_cluster.fiap_cluster.vpc_config[0].subnet_ids
+  security_groups    = [aws_security_group.eks_lb_sg.id]
+
+  tags = {
+    Name = "eks-lb"
+  }
+}
+
+resource "aws_lb_target_group" "eks_target_group" {
+  name     = "eks-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_eks_cluster.fiap_cluster.vpc_config[0].vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = 80
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "eks_lb_listener" {
+  load_balancer_arn = aws_lb.eks_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.eks_target_group.arn
+    type             = "forward"
+  }
+}
+
+
 #provider "aws" {
 #  region = "us-east-1"
 #}
