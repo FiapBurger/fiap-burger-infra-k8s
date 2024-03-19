93331e89a61ee008f6d5dd7380118e4a010453fe
Fiapburger01 <fiapburger.tech@gmail.com>
2024-03-18 22:58:30 -0300

    feat: cluster name fiap-burger-eks-cluster

--- main.tf
+++ main.tf
@@ -67,3 +67,49 @@ resource "aws_lb_listener" "eks_lb_listener" {
     type             = "forward"
   }
 }
+
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "example-api"
+  description = "Exemplo de API Gateway"
+}
+
+resource "aws_api_gateway_resource" "example_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "example"
+}
+
+resource "aws_api_gateway_method" "example_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.example_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "example_integration" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example_resource.id
+  http_method = aws_api_gateway_method.example_method.http_method
+
+  type                    = "HTTP_PROXY"
+  integration_http_method = "GET"
+  uri                     = "http://${aws_lb.eks_lb.dns_name}/produtos"  # Substitua pelo seu endpoint Kubernetes
+}
+
+resource "aws_api_gateway_method_response" "example_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example_resource.id
+  http_method = aws_api_gateway_method.example_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "example_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example_resource.id
+  http_method = aws_api_gateway_method.example_method.http_method
+  status_code = aws_api_gateway_method_response.example_method_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
